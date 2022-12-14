5d22e38ba5dcd6655f9057acd6cb821cd226e07d
Kakucharz <116747792+Kakucharz@users.noreply.github.com>
2022-12-14 18:12:18 +0100

    Update seaborn.py
    przerobiłam na tabelkę z naszego kodu

--- seaborn.py
+++ seaborn.py
@@ -1,9 +1,9 @@
 import pandas as pd
-file_errors_location = 'C:\\Users\\Karolina\\Documents\\book1.xlsx'
+file_errors_location = 'output.xlsx'
 df = pd.read_excel(file_errors_location)
 print(df)
 
 from matplotlib import pyplot as plt
 import seaborn as sns
-sns.lmplot(x= 'Waga', y='Wzrost', data=df,)
+sns.lmplot(x= 'Lp.', y='BMI', data=df,)
 sns.color_palette("Spectral", as_cmap=True)
